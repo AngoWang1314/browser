50ad765133d3a93f736234463a50d7b9cc412706
kayanchan <1010136337@qq.com>
2019-08-12 14:01:11 +0800

    untis.js

--- build/utils.js
+++ build/utils.js
@@ -58,7 +58,14 @@ exports.cssLoaders = function (options) {
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
-    less: generateLoaders('less'),
+    less: generateLoaders('less').concat({
+      loader: 'sass-resources-loader',
+      options: {
+          resources: [
+              path.resolve(__dirname, '../src/styles/less/variables.less')
+          ]
+      }
+    }),
     sass: generateLoaders('sass', { indentedSyntax: true }),
     scss: generateLoaders('sass'),
     stylus: generateLoaders('stylus'),
